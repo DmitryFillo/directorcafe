b30515cc8b8a0ce669056a13b46eff80b501ce2b
DmitryFillo <fillo@fillo.me>
2017-11-22 17:38:54 +0300

    infrastructure for tests added

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testMatch: [
+    "**/__tests__/**/*.spec.js"
+  ],
+  collectCoverage: true,
+  coverageReporters: [
+    'lcov',
+  ],
+};
---
+++ src/utils/__tests__/replayGenerator.spec.js
@@ -0,0 +1,7 @@
+// @flow
+
+import replayGenerator from '../replayGenerator';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(3).toBe(3);
+});
